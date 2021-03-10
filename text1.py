1b98ecffacb020d8268bf9cdad808a5a1a9c2edc
yaogu <479690607@qq.com>
2021-03-10 23:46:43 +0800

    第一个代码

---
+++ text1.py
@@ -0,0 +1,13 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2021/2/21 18:39
+# @Author : CZC
+# @File : text1.py
+# @Softwore: PyCharm
+
+import time
+
+while True:
+    time.sleep(1)
+    print("hello")
+
+
